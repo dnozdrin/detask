eb9a986aee39931efcb1f886d7ac40fca22a6f94
Dmytro Nozdrin <nozdrin.dmytro@gmail.com>
2020-06-23 09:58:03 +0300

    Improve validation errors marshal json tests

--- internal/domain/validation/validation_test.go
+++ internal/domain/validation/validation_test.go
@@ -37,9 +37,34 @@ func TestErrorsAdd(t *testing.T) {
 }
 
 func TestMarshalJSON(t *testing.T) {
-	e := NewErrors()
-	e.errors = append(e.errors, Error{Field: "name", Message: "incorrect"})
-	result, err := e.MarshalJSON()
-	assert.Nil(t, err)
-	assert.Equal(t, []byte("{\"error\":\"validation failed\",\"errors\":[{\"field\":\"name\",\"message\":\"incorrect\"}]}"), result)
+	tests := []struct {
+		name   string
+		errors []Error
+		json []byte
+	}{
+		{
+			name: "no_errors",
+			errors: []Error{},
+			json: []byte("{\"error\":\"validation failed\",\"errors\":[]}"),
+		},
+		{
+			name: "1_error",
+			errors: []Error{{Field: "name", Message: "incorrect"}},
+			json: []byte("{\"error\":\"validation failed\",\"errors\":[{\"field\":\"name\",\"message\":\"incorrect\"}]}"),
+		},
+		{
+			name: "2_errors",
+			errors: []Error{{Field: "dummy", Message: "error"}, {Field: "tests", Message: "test"}},
+			json: []byte("{\"error\":\"validation failed\",\"errors\":[{\"field\":\"dummy\",\"message\":\"error\"},{\"field\":\"tests\",\"message\":\"test\"}]}"),
+		},
+	}
+	for _, test := range tests {
+		t.Run("validation_"+test.name, func(t *testing.T) {
+			e := NewErrors()
+			e.errors = test.errors
+			result, err := e.MarshalJSON()
+			assert.Nil(t, err)
+			assert.Equal(t, test.json, result)
+		})
+	}
 }
